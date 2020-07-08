6940d0f2a3091308b5abe683f0d4620a524eb8fa
Jason <jason.jafari.m@gmail.com>
2020-07-08 14:20:18 -0400

    for last commit

--- wp-content/plugins/Jason-Plugin1/uninstall.php
+++ wp-content/plugins/Jason-Plugin1/uninstall.php
@@ -16,9 +16,8 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 
 
 //Clear Database stored data
-// $books = get_posts(array('post_type' => 'book', 'numberposts' => -1));
+$books = get_posts(array('post_type' => 'book', 'numberposts' => -1));
 
-// foreach ($books as $book) {
-//   wp_delete_post($book->ID, true);
-// }
-
+foreach ($books as $book) {
+  wp_delete_post($book->ID, true);
+}
