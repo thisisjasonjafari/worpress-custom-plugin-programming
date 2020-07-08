9eb59bc9d7b912b4feff75c6716265287c399967
Jason <jason.jafari.m@gmail.com>
2020-07-08 00:24:29 -0400

    some change in plugin

--- wp-content/plugins/Jason-Plugin1/index.php
+++ wp-content/plugins/Jason-Plugin1/index.php
@@ -1,17 +1,3 @@
 <?php
-
-/**
- * @pakege Jason-Custom-Plugin
- */
-
-/*
-Plugin Name:  jason Plugin custom 001
-Plugin URI: https://github.com/thisisjasonjafari/worpress-custom-plugin-programming
-Description: Plugin Description
-Version: 1.0.1
-Author: Jason Jafari
-Author URI: http://jasonjafari.com
-License: GPLv2 or later
-text-Domain: Jason-plugin
-*/
-
+ 
+// Silence is gilden for preventing show content of folder
