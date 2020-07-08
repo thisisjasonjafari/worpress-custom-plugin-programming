e8c25c73ce30b5b4b810b7efcdf23174a5bec61c
Jason <jason.jafari.m@gmail.com>
2020-07-08 00:15:00 -0400

    wite first plugin

--- wp-content/plugins/Jason-Plugin1/index.php
+++ wp-content/plugins/Jason-Plugin1/index.php
@@ -1,1 +1,17 @@
- sdfsdf
+<?php
+
+/**
+ * @pakege Jason-Custom-Plugin
+ */
+
+/*
+Plugin Name:  jason Plugin custom 001
+Plugin URI: https://github.com/thisisjasonjafari/worpress-custom-plugin-programming
+Description: Plugin Description
+Version: 1.0.1
+Author: Jason Jafari
+Author URI: http://jasonjafari.com
+License: GPLv2 or later
+text-Domain: Jason-plugin
+*/
+
